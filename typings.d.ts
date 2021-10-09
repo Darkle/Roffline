5401b3bc3dffe86c068df3227c0fe05e8f609b38
Coop <coop.coding@gmail.com>
2021-10-09 17:23:21 +1100

    Update

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,13 @@
+// import { LoginPageReturnType } from './frontend/js/login'
+
 declare module 'fastify-no-additional-properties'
 declare module 'fastify-error-page'
 declare module 'fastify-disablecache'
+
+// /* eslint-disable functional/prefer-type-literal*/
+
+// declare global {
+//   interface Window {
+//     loginPage: () => LoginPageReturnType
+//   }
+// }
