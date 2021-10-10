51d9d3697fb3570b58896f9e5c828c3881609926
Coop <coop.coding@gmail.com>
2021-10-10 15:14:08 +1100

    todos

--- server/routes/api-router.ts
+++ server/routes/api-router.ts
@@ -1,6 +1,7 @@
 // import { FastifyInstance } from 'fastify'
 
 // TODO: make sure to do a check they are logged in if not already as this is a new prefix route
+//TODO: also dont forget to add onRequest: fastify.csrfProtection to each POST route
 
 // const apiRoutes = (fastify: FastifyInstance, _: unknown, done: (err?: Error) => void): void => {
 //   fastify.get('/user', handler_v1)
