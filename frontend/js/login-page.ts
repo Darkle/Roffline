ef1bf6fcc8dd1178e9b93c49f143c933968d37c3
Coop <coop.coding@gmail.com>
2021-10-12 10:11:58 +1100

    Got login page working

--- frontend/js/login-page.ts
+++ frontend/js/login-page.ts
@@ -1,33 +1,24 @@
-import Vue from 'vue'
+import * as Vue from 'vue'
 
-const Counter = {
-  data(): { counter: number } {
+const Counter = Vue.defineComponent({
+  data() {
     return {
-      counter: 0,
+      userIsLoggingIn: true,
     }
   },
-}
+  mounted() {
+    document.querySelector('.signup-form')?.removeAttribute('x-cloak')
+  },
+  methods: {
+    setUserIsLoggingIn(val: boolean) {
+      this.userIsLoggingIn = val
+    },
+    copyNewUsernameToClipboard() {
+      // eslint-disable-next-line @typescript-eslint/no-extra-semi
+      ;(document.querySelector('#signupUsernameInput') as HTMLInputElement)?.select()
+      document.execCommand('copy')
+    },
+  },
+})
 
-Vue.createApp(Counter).mount('#counter')
-
-// function copyNewUsernameToClipboard(): void {
-//   ;(this as Alpine).$refs.signupUsernameInput.select()
-//   document.execCommand('copy')
-// }
-
-// // // eslint-disable-next-line functional/prefer-type-literal
-// // interface WindowWithLoginPageType extends Window {
-// //   loginPage: () => LoginPageReturnType
-// // }
-
-// type LoginPageReturnType = {
-//   userIsLoggingIn: boolean
-//   copyNewUsernameToClipboard: () => void
-// }
-
-// window.loginPage = (): LoginPageReturnType => ({
-//   userIsLoggingIn: true,
-//   copyNewUsernameToClipboard,
-// })
-
-// export { LoginPageReturnType }
+Vue.createApp(Counter).mount('body')
