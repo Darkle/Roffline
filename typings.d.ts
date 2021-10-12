7d610cb7092d5b485409662d656de54c3000fa56
Coop <coop.coding@gmail.com>
2021-10-12 17:13:00 +1100

    Added typings for js-ago

--- typings.d.ts
+++ typings.d.ts
@@ -1,8 +1,15 @@
-// import { LoginPageReturnType } from './frontend/js/login'
-
 declare module 'fastify-no-additional-properties'
 declare module 'fastify-error-page'
 declare module 'fastify-disablecache'
+
+declare module 'js-ago' {
+  export default function jsAgo(
+    timestamp: Date | number,
+    options?: {
+      format: 'short' | 'medium' | 'long'
+    }
+  ): string
+}
 
 /* eslint-disable functional/prefer-type-literal*/
 
