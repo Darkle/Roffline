c5abbee91e7d8b3253eeccd88626deacace103b5
Coop <coop.coding@gmail.com>
2021-10-13 15:13:54 +1100

    Update

--- server/routes/api-router.ts
+++ server/routes/api-router.ts
@@ -2,6 +2,7 @@
 
 // TODO: make sure to do a check they are logged in if not already as this is a new prefix route
 //TODO: also dont forget to add onRequest: fastify.csrfProtection to each POST route
+//TODO: remember to make sure the csrf and logged in prehandlers come before other prehandlers
 
 // const apiRoutes = (fastify: FastifyInstance, _: unknown, done: (err?: Error) => void): void => {
 //   fastify.get('/user', handler_v1)
