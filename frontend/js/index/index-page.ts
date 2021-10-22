93a8f83b01177986daa37463d0a1e03e8ff2d2f2
Coop <coop.coding@gmail.com>
2021-10-22 14:08:29 +1100

    Made updatePosts a bit clearer

--- frontend/js/index/index-page.ts
+++ frontend/js/index/index-page.ts
@@ -1,6 +1,9 @@
 import * as Vue from 'vue'
 import VueSplide from '@splidejs/vue-splide'
 import * as R from 'ramda'
+import Clipboard from 'v-clipboard'
+import { compose } from 'ts-functional-pipe'
+import * as RA from 'ramda-adjunct'
 
 import { FrontendPost, WindowWithProps } from '../frontend-global-types'
 import { PostItem } from './components/PostItem'
@@ -19,9 +22,8 @@ const IndexPage = Vue.defineComponent({
   methods: {
     updatePosts(posts: FrontendPost[]) {
       // Sort the posts by created_utc for the off chance that two fetch requests happen in quick succession and they are returned out of order.
-      const sortNewestPostsFirst = R.sortBy(R.prop('created_utc'))
-      // eslint-disable-next-line functional/immutable-data
-      this.posts = sortNewestPostsFirst([...this.posts, ...posts]).reverse()
+      const sortNewestPostsFirst = compose(p => p.reverse(), RA.sortByProps(['created_utc'])) // eslint-disable-line functional/immutable-data
+      this.posts = sortNewestPostsFirst([...this.posts, ...posts]) as FrontendPost[]
     },
   },
   emits: ['updatePosts'],
@@ -35,4 +37,4 @@ const app = Vue.createApp(IndexPage)
 // warnHandler is ignored in production https://v3.vuejs.org/api/application-config.html#warnhandler
 app.config.warnHandler = ignoreScriptTagCompilationWarnings
 
-app.use(VueSplide).mount('body')
+app.use(VueSplide).use(Clipboard).mount('body')
