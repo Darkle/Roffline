5bf68c4babd99542f580f357b1d8ee3f0f435545
Coop <coop.coding@gmail.com>
2021-10-23 16:23:29 +1100

    Got basic updates for settings page working

--- frontend/js/settings-page.ts
+++ frontend/js/settings-page.ts
@@ -1,17 +1,38 @@
 import * as Vue from 'vue'
 
 import { SettingsPageWindowWithProps } from './frontend-global-types'
-import { ignoreScriptTagCompilationWarnings } from './frontend-utils'
+import { Fetcher, ignoreScriptTagCompilationWarnings } from './frontend-utils'
 
 declare const window: SettingsPageWindowWithProps
 
 const SettingsPage = Vue.defineComponent({
   data() {
     return {
-      userSettings: window.userSettings,
+      name: window.name,
+      subreddits: window.userSettings.subreddits,
+      hideStickiedPosts: window.userSettings.hideStickiedPosts,
+      onlyShowTitlesInFeed: window.userSettings.onlyShowTitlesInFeed,
+      infiniteScroll: window.userSettings.infiniteScroll,
+      darkModeTheme: window.userSettings.darkModeTheme,
     }
   },
-  methods: {},
+  methods: {
+    updateSetting(event: Event) {
+      const inputElement = event.target as HTMLInputElement
+      const { settingName } = inputElement.dataset
+
+      const data = { settingName, settingValue: inputElement.checked }
+
+      Fetcher.putJSON('/api/update-user-setting', data).catch(err => console.error(err))
+    },
+    updateDarkTheme(event: Event) {
+      this.updateSetting(event)
+      const inputElement = event.target as HTMLInputElement
+      const body = document.querySelector('body') as HTMLBodyElement
+
+      inputElement.checked ? body.classList.add('dark-theme') : body.classList.remove('dark-theme')
+    },
+  },
 })
 
 const app = Vue.createApp(SettingsPage)
