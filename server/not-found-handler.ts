92fcf91a37393fe2c51c19b00b43594ec6a25df1
Coop <coop.coding@gmail.com>
2021-11-06 18:46:41 +1100

    Extracted the route checks into descriptive functions

--- server/not-found-handler.ts
+++ server/not-found-handler.ts
@@ -3,17 +3,21 @@ import { StatusCodes as HttpStatusCode } from 'http-status-codes'
 
 import { mainLogger } from '../logging/logging'
 
+const isAnApiRoute = (url: string): boolean => url.startsWith('/admin/api/') || url.startsWith('/api/')
+
+const isAnAdminPage = (url: string): boolean => url.startsWith('/admin/')
+
 function notFoundHandler(req: FastifyRequest, reply: FastifyReply): void {
   mainLogger.error(`404, page not found: ${req.url}`)
 
   // eslint-disable-next-line functional/no-conditional-statement
-  if (req.url.startsWith('/admin/api/') || req.url.startsWith('/api/')) {
+  if (isAnApiRoute(req.url)) {
     reply.code(HttpStatusCode.NOT_FOUND).send()
     return
   }
 
   // eslint-disable-next-line functional/no-conditional-statement
-  if (req.url.startsWith('/admin/')) {
+  if (isAnAdminPage(req.url)) {
     reply.code(HttpStatusCode.NOT_FOUND).send(`404 - page not found`)
     return
   }
