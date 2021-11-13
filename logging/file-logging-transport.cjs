9c8e72bfb287e32e14225fc1b4ba9f0f931c87ee
Coop <coop.coding@gmail.com>
2021-11-13 15:40:42 +1100

    Fixed filename issue

--- logging/file-logging-transport.cjs
+++ logging/file-logging-transport.cjs
@@ -8,14 +8,31 @@ const rotatingFileStream = require('rotating-file-stream')
 
 const maxFiles = 5
 
+const pad = num => (num > 9 ? '' : '0') + num
+
+const logFilenameGenerator = (time, index) => {
+  // eslint-disable-next-line functional/no-conditional-statement,no-param-reassign
+  if (!time) time = new Date()
+  // eslint-disable-next-line functional/no-conditional-statement,no-param-reassign
+  if (!index) index = 0
+
+  const month = `${time.getFullYear()}${pad(time.getMonth() + 1)}`
+  const day = pad(time.getDate())
+  const hour = pad(time.getHours())
+  const minute = pad(time.getMinutes())
+
+  return `${month}${day}-${hour}${minute}-${index}-roffline.log`
+}
+
 /**
  * @param {{ outDir: string }}  options
  */
 // eslint-disable-next-line functional/immutable-data
 module.exports = function (options) {
-  return rotatingFileStream.createStream(path.join(options.outDir, 'roffline.log'), {
-    size: '5M',
+  return rotatingFileStream.createStream(logFilenameGenerator, {
+    size: '3M',
     interval: '2d',
     maxFiles,
+    path: options.outDir,
   })
 }
