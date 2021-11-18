a1f00016b156306f0a62c717b3830ddfd650f58a
Coop <coop.coding@gmail.com>
2021-11-18 17:46:01 +1100

    Working on update scheduler

---
+++ downloads/feeds/fetch-feeds.ts
@@ -0,0 +1,1 @@
+generateAndFetchSubredditsFeeds(adminSettings, subsToUpdate).then(batchAddSubredditsPostIdReferences)
--- downloads/update-scheduler.ts
+++ downloads/update-scheduler.ts
@@ -8,15 +8,18 @@ import { isOffline } from './check-if-offline'
 
 const thirtySecondsInMs = 30000
 
-/* eslint-disable functional/no-conditional-statement,functional/no-try-statement,functional/no-let,complexity,array-bracket-newline */
+/* eslint-disable functional/no-conditional-statement,functional/no-try-statement,functional/no-let,complexity,array-bracket-newline, max-lines-per-function */
 
 let timer: ReturnType<typeof global.setTimeout>
 let downloadsAreRunning = false
+let adminSettingsCache: AdminSettings
 
-const feedsToBeFetched = new Set()
-const postsToBeRetrieved = new Set()
-const commentsToRetrieved = new Set()
-const postsMediaToBeDownloaded = new Set()
+const downloadsStore = {
+  subsToUpdate: new Set(),
+  postsToBeRetrieved: new Set(),
+  commentsToRetrieved: new Set(),
+  postsMediaToBeDownloaded: new Set(),
+}
 
 function shouldRunNewUpdate({ updateAllDay, updateStartingHour, updateEndingHour }: AdminSettings): boolean {
   if (updateAllDay) return true
@@ -29,39 +32,91 @@ function shouldRunNewUpdate({ updateAllDay, updateStartingHour, updateEndingHour
   return currentHour > updateStartingHour && currentHour < updateEndingHour
 }
 
-function addThingsToBeDownloadedToSetStores(
-  feeds: string[],
+function addThingsToBeDownloadedToDownloadsStore([subs, postsToGet, commentsToGet, mediaToGet]: [
+  subs: SubredditsMasterList[],
   postsToGet: PostsToGet[],
   commentsToGet: Post[],
   mediaToGet: Post[]
-): void {
-  feeds.forEach(feed => {
-    feedsToBeFetched.add(feed)
+]): void {
+  // Since these are Set's, duplicates are ignored.
+  subs.forEach(({ subreddit }) => {
+    downloadsStore.subsToUpdate.add(subreddit)
   })
 
   postsToGet.forEach(({ id }) => {
-    postsToBeRetrieved.add(id)
+    downloadsStore.postsToBeRetrieved.add(id)
   })
 
   commentsToGet.forEach(({ id }) => {
-    commentsToRetrieved.add(id)
+    downloadsStore.commentsToRetrieved.add(id)
   })
 
   mediaToGet.forEach(({ id }) => {
-    postsMediaToBeDownloaded.add(id)
+    downloadsStore.postsMediaToBeDownloaded.add(id)
   })
 }
 
-// eslint-disable-next-line max-lines-per-function
+function removeSuccessfullDownloadsFromDownloadStore(downloadedItems, downloadsType) {
+  downloadsStore
+}
+
+const moreSubsToUpdate = (): boolean => downloadsStore.subsToUpdate.size > 0
+const morePostsToBeRetrieved = (): boolean => downloadsStore.postsToBeRetrieved.size > 0
+const moreCommentsToRetrieved = (): boolean => downloadsStore.commentsToRetrieved.size > 0
+const morePostsMediaToBeDownloaded = (): boolean => downloadsStore.postsMediaToBeDownloaded.size > 0
+const moreToDownload = (): boolean =>
+  moreSubsToUpdate() || morePostsToBeRetrieved() || moreCommentsToRetrieved() || morePostsMediaToBeDownloaded()
+
+function startSomeDownloads(): Promise<void> {
+  if (moreSubsToUpdate()) {
+    return fetchFeeds(adminSettingsCache, downloadsStore.subsToUpdate)
+      .then(updateSubsLastUpdate)
+      .then(subsUpdated => removeSuccessfullDownloadsFromDownloadStore(subsUpdated, 'subsToUpdate'))
+  }
+
+  // if (morePostsToBeRetrieved()) {
+  //   return updateSubredditsFeeds(adminSettingsCache, downloadsStore.postsToBeRetrieved)
+  // .then(postsRetrieved => removeSuccessfullDownloadFromDownloadStore(postsRetrieved, 'postsToBeRetrieved'))
+  // }
+
+  // if (moreCommentsToRetrieved()) {
+  //   return updateSubredditsFeeds(adminSettingsCache, downloadsStore.commentsToRetrieved)
+  // .then(commentsRetrieved => removeSuccessfullDownloadFromDownloadStore(commentsRetrieved, 'commentsToRetrieved'))
+  // }
+
+  // if (morePostsMediaToBeDownloaded()) {
+  //   return updateSubredditsFeeds(adminSettingsCache, downloadsStore.postsMediaToBeDownloaded)
+  // .then(mediaDownloads => removeSuccessfullDownloadFromDownloadStore(mediaDownloads, 'postsMediaToBeDownloaded'))
+  // }
+
+  // Dont forget to do the concurrency dynamically based on the admin settings
+  // When downloading, need to send through the admin settings with that call as will want to know if downloading videos has changed et.al.
+  // So on download completed, we check if there is anything more to do in the set()s, if not, we set downloadsAreRunning to false
+  // On download completed of feeds, make sure to first clear that subs table first before store all the new data
+  // Also on download complete, we update these things:
+  // - lastUpdate for a sub in subreddits_master_list needs to be updated with new date for each sub after finished getting all its feeds
+  //   - And need to remove it from the feedsToBeFetched Set()
+  // - remove post id from posts_to_get table
+  // - commentsDownloaded for a post in posts table needs to be set to true
+  //   - And need to remove it from the commentsToBeRetrieved Set()
+  // - mediaDownloadTries for a post needs to be updated in posts table regardless of success
+  //   - On successful update, set media_has_been_downloaded to true in posts table
+  // Dont forget to update post_to_get and remove posts we have gotten.
+  // Catch and log individual downloads inside their respective downloaders as a single failed download shouldnt bubble up to here
+
+  return moreToDownload() ? startSomeDownloads() : Promise.resolve()
+}
+
 function scheduleUpdates(): void {
-  // eslint-disable-next-line max-lines-per-function
   timer = setTimeout(() => {
     clearTimeout(timer)
     // Wait for any file IO in the event loop to finish before starting
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises,max-lines-per-function
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     setImmediate(async () => {
       try {
         const [adminSettings, weAreOffline] = await Promise.all([db.getAdminSettings(), isOffline()])
+
+        adminSettingsCache = adminSettings
 
         if (weAreOffline || !shouldRunNewUpdate(adminSettings)) return
 
@@ -69,30 +124,7 @@ function scheduleUpdates(): void {
 
         mainLogger.trace(`New update. Starting at: ${now.toString()}`)
 
-        const [subsToUpdate, postsToGet, commentsToGet, mediaToGet] = await db.getThingsThatNeedToBeDownloaded()
-
-        const feeds = await thing(subsToUpdate)
-
-        addThingsToBeDownloadedToSetStores(feeds, postsToGet, commentsToGet, mediaToGet)
-
-        // check subreddits_master_list table if any subs feeds need updating, if there are, generate them and add them to feedsToBeFetched
-        // check post_to_get if any posts need to be got and add them to postsToBeRetrieved
-        // check posts table if any comments still need to be gotten and add them to commentsToRetrieved
-        // check posts table to check if any posts media needs to be downloaded (dont forget to check max download tries too) and add them to postsMediaToBeDownloaded
-        // If there are subs to update, generate those feeds
-        //So when downloading and checking getting something to download next, we favour feedsToBeFetched > postsToBeRetrieved > commentsToRetrieved > postsMediaToBeDownloaded
-        // Dont forget to do the concurrency dynamically based on the admin settings
-        // When downloading, need to send through the admin settings with that call as will want to know if downloading videos has changed et.al.
-        // So on download completed, we check if there is anything more to do in the set()s, if not, we set downloadsAreRunning to false
-        // Also on download complete, we update these things:
-        // - lastUpdate for a sub in subreddits_master_list needs to be updated with new date for each sub after finished getting all its feeds
-        //   - And need to remove it from the feedsToBeFetched Set()
-        // - remove post id from posts_to_get table
-        // - commentsDownloaded for a post in posts table needs to be set to true
-        //   - And need to remove it from the commentsToBeRetrieved Set()
-        // - mediaDownloadTries for a post needs to be updated in posts table regardless of success
-        //   - On successful update, set media_has_been_downloaded to true in posts table
-        // Dont forget to update post_to_get and remove posts we have gotten.
+        await db.getThingsThatNeedToBeDownloaded().then(addThingsToBeDownloadedToDownloadsStore)
 
         if (!downloadsAreRunning) {
           downloadsAreRunning = true
