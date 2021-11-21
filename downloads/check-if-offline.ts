83b2e58a3feaab0fe7140edabe2ad298c51e4981
Coop <coop.coding@gmail.com>
2021-11-21 14:16:17 +1100

    Added error catch to isOffline check

--- downloads/check-if-offline.ts
+++ downloads/check-if-offline.ts
@@ -1,7 +1,15 @@
+import fetch from 'node-fetch-commonjs'
+
+import { mainLogger } from '../logging/logging'
+
 const offlineCheckerUrl = process.env['OFFLINE_CHECK_URL'] as string
 
-function isOffline(): Promise<boolean> {
-  return fetch(offlineCheckerUrl, { method: 'HEAD' }).then(resp => !resp.ok)
-}
+const isOffline = (): Promise<boolean> =>
+  fetch(offlineCheckerUrl, { method: 'HEAD' })
+    .then(resp => !resp.ok)
+    .catch(err => {
+      mainLogger.trace(err)
+      return true
+    })
 
 export { isOffline }
