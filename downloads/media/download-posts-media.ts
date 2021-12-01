45483f7f7324d01bb20fc65f65c7c9947cb8104f
Coop <coop.coding@gmail.com>
2021-12-01 12:08:59 +1100

    Fixed Prray to be mapAsync. Added skipDownload

--- downloads/media/download-posts-media.ts
+++ downloads/media/download-posts-media.ts
@@ -6,21 +6,30 @@ import RA from 'ramda-adjunct'
 import { db } from '../../db/db'
 import { AdminSettings } from '../../db/entities/AdminSettings'
 import { Post } from '../../db/entities/Posts/Post'
+import { mediaDownloadsLogger } from '../../logging/logging'
 import { getEnvFilePath, pCreateFolder, isNotError } from '../../server/utils'
 import { DownloadsStore } from '../downloads-store'
 import { downloadDirectMediaLink } from './direct-media-download'
+import { getUrlFromTextPost } from './get-url-from-text-post'
+import { logDownloadErrorIfNotOffline } from './log-download-errors'
 import { adminMediaDownloadsViewerOrganiser } from './media-downloads-viewer-organiser'
 import {
   isDirectMediaLink,
   isCrossPost,
   isTextPost,
-  isVideoPost,
-  isImagePost,
+  // isVideoPost,
+  // isImagePost,
   isTextPostWithNoUrlInPost,
-  isNotRedditUrl,
+  // isNotRedditUrl,
 } from './posts-media-categorizers'
 
 type PostId = string
+
+type MediaDownload = {
+  post: Post & { isTextPostWithNoUrlsInPost?: boolean }
+  adminSettings: AdminSettings
+  postMediaFolder: string
+}
 
 const postsMediaContainerFolder = getEnvFilePath(process.env['POSTS_MEDIA_DOWNLOAD_DIR'])
 const maxNumberDownloadTriesAllowed = 3
@@ -33,8 +42,12 @@ const createMediaFolderForPost = (postId: string): Promise<string> => {
   return pCreateFolder(postMediaFolder).then(_ => postMediaFolder)
 }
 
+const skipDownload = R.curry((skipReason: string, { post }: { post: Post }) => {
+  adminMediaDownloadsViewerOrganiser.setDownloadCancelled(post.id, skipReason)
+  mediaDownloadsLogger.info(`Didn't download this post: ${skipReason}`, { ...post, skipReason })
+})
+
 const downloadIndividualPostMedia = R.compose(
-  // eslint-disable-next-line ramda/cond-simplification
   R.cond([
     [isDirectMediaLink, downloadDirectMediaLink],
     // [isImagePost, downloadImage],
@@ -46,17 +59,17 @@ const downloadIndividualPostMedia = R.compose(
     //     skipDownload('Video downloads disabled')
     //   ),
     // ],
-    // [isTextPostWithNoUrlInPost, skipDownload('Is a text-post with no url in post')],
+    [isTextPostWithNoUrlInPost, skipDownload('Is a text-post with no url in post')],
     // [isNotRedditUrl, saveWebPage],
-    // /*****
-    //  Ignore crossposts for now where the url links to another post (eg https://www.reddit.com/r/...)
-    //  Leave the crossposts check towards the end of the checks as it is sometimes possible to download
-    //   the video or image of a crosspost if the url is not a https://www.reddit.com/r/ url.
-    // *****/
-    // [isCrossPost, skipDownload('Is a cross-post with no direct download url')],
-    // [R.T, skipDownload('No media match for download.')],
-  ])
-  // R.when(isTextPost, getUrlFromTextPost)
+    /*****
+     Ignore crossposts for now where the url links to another post (eg https://www.reddit.com/r/...)
+     Leave the crossposts check towards the end of the checks as it is sometimes possible to download
+      the video or image of a crosspost if the url is not a https://www.reddit.com/r/ url.
+    *****/
+    [isCrossPost, skipDownload('Is a cross-post with no direct download url')],
+    [R.T, skipDownload('No media match for download.')],
+  ]),
+  R.when(isTextPost, getUrlFromTextPost)
 )
 
 const removeFailedDownloads = (items: (PostId | undefined | Error)[]): PostId[] | [] =>
@@ -71,59 +84,58 @@ function downloadPostsMedia(
 
   adminMediaDownloadsViewerOrganiser.initializeWithNewPosts(postsArr)
 
-  return (
-    Prray.from(postsArr)
-      .forEachAsync(
-        // eslint-disable-next-line max-lines-per-function
-        async (post: Post): Promise<PostId | Error | undefined> => {
-          // eslint-disable-next-line functional/no-conditional-statement
-          if (tooManyDownloadTries(post)) {
-            adminMediaDownloadsViewerOrganiser.setDownloadCancelled(
-              post.id,
-              'Download Skipped: Too many download tries (3).'
-            )
-            return
-          }
-
-          adminMediaDownloadsViewerOrganiser.incrementPostMediaDownloadTry(post.id)
-
-          await db.incrementPostMediaDownloadTry(post.id)
-
-          const postMediaFolder = await createMediaFolderForPost(post.id)
-
-          // eslint-disable-next-line functional/no-try-statement
-          try {
-            adminMediaDownloadsViewerOrganiser.setDownloadStarted(post.id)
-
-            await downloadIndividualPostMedia({
-              post,
-              adminSettings,
-              postMediaFolder,
-            })
-
-            adminMediaDownloadsViewerOrganiser.setDownloadSucceeded(post.id)
-
-            await db.setMediaDownloadedTrueForPost(post.id)
-
-            return post.id
-          } catch (err) {
-            //To appease the Typescript gods: https://github.com/microsoft/TypeScript/issues/20024
-            const downloadError = err as Error
-
-            adminMediaDownloadsViewerOrganiser.setDownloadFailed(post.id, downloadError)
-
-            await logDownloadErrorIfNotOffline(err, post)
-
-            return downloadError
-          }
-        },
-        {
-          concurrency: adminSettings.numberMediaDownloadsAtOnce,
+  return Prray.from(postsArr)
+    .mapAsync(
+      // eslint-disable-next-line max-lines-per-function
+      async (post: Post) => {
+        // eslint-disable-next-line functional/no-conditional-statement
+        if (tooManyDownloadTries(post)) {
+          adminMediaDownloadsViewerOrganiser.setDownloadCancelled(
+            post.id,
+            'Download Skipped: Too many download tries (3).'
+          )
+          return
         }
-      )
-      // .then((items: (PostId | undefined | Error)[]): PostId[] => removeFailedDownloads(items) as PostId[])
-      .then(removeFailedDownloads)
-  )
+
+        adminMediaDownloadsViewerOrganiser.incrementPostMediaDownloadTry(post.id)
+
+        await db.incrementPostMediaDownloadTry(post.id)
+
+        const postMediaFolder = await createMediaFolderForPost(post.id)
+
+        // eslint-disable-next-line functional/no-try-statement
+        try {
+          adminMediaDownloadsViewerOrganiser.setDownloadStarted(post.id)
+
+          const mediaDownload: MediaDownload = {
+            post,
+            adminSettings,
+            postMediaFolder,
+          }
+
+          await downloadIndividualPostMedia(mediaDownload)
+
+          adminMediaDownloadsViewerOrganiser.setDownloadSucceeded(post.id)
+
+          await db.setMediaDownloadedTrueForPost(post.id)
+
+          return post.id
+        } catch (err) {
+          //To appease the Typescript gods: https://github.com/microsoft/TypeScript/issues/20024
+          const downloadError = err as Error
+
+          adminMediaDownloadsViewerOrganiser.setDownloadFailed(post.id, downloadError)
+
+          await logDownloadErrorIfNotOffline(downloadError, post)
+
+          return downloadError
+        }
+      },
+      {
+        concurrency: adminSettings.numberMediaDownloadsAtOnce,
+      }
+    )
+    .then(removeFailedDownloads)
 }
 
 export { downloadPostsMedia }
