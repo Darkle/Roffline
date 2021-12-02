6d1f7b1d4a48554fd29a4211993d3eccb98895e2
Coop <coop.coding@gmail.com>
2021-12-02 17:25:05 +1100

    Changed skip download logger to trace as dont really need that logged

--- downloads/media/download-media-decider.ts
+++ downloads/media/download-media-decider.ts
@@ -1,20 +1,17 @@
-import * as R from 'ramda'
 import { AdminSettings } from '../../db/entities/AdminSettings'
 
 import { Post } from '../../db/entities/Posts/Post'
 import { mediaDownloadsLogger } from '../../logging/logging'
 import { downloadDirectMediaLink } from './direct-media-download'
 import { savePageAsPdf } from './download-webpage'
-import { getUrlFromTextPost } from './get-url-from-text-post'
 import { adminMediaDownloadsViewerOrganiser } from './media-downloads-viewer-organiser'
 import {
   isArticleToSaveAsPdf,
   isCrossPost,
   isDirectMediaLink,
-  // isImagePost,
-  isTextPost,
+  isImagePost,
   isTextPostWithNoUrlInPost,
-  // isVideoPost,
+  isVideoPost,
 } from './posts-media-categorizers'
 
 type MediaDownload = {
@@ -25,47 +22,63 @@ type MediaDownload = {
 
 const skipDownload = (skipReason: string, post: Post): Promise<void> => {
   adminMediaDownloadsViewerOrganiser.setDownloadCancelled(post.id, skipReason)
-  mediaDownloadsLogger.info(`Didn't download this post: ${skipReason}`, { ...post, skipReason })
+  mediaDownloadsLogger.trace(`Didn't download this post: ${skipReason}`, { ...post, skipReason })
   return Promise.resolve()
 }
 
 /* eslint-disable functional/no-conditional-statement,max-lines-per-function,complexity */
 
-function downloadIndividualPostMedia({ post, adminSettings, postMediaFolder }: MediaDownload): Promise<void> {
-  R.when(isTextPost, getUrlFromTextPost)
+function downloadIndividualPostMedia({ post, postMediaFolder }: MediaDownload): Promise<void> {
+  debugger
   if (isDirectMediaLink(post)) {
+    debugger
     return downloadDirectMediaLink(post, postMediaFolder)
   }
 
-  // if (isImagePost(post)) {
-  //   return downloadImage({ post, adminSettings, postMediaFolder })
-  // }
+  debugger
 
-  // if (isVideoPost(post)) {
-  //   return R.ifElse(
-  //     R.pathEq(['settings', 'downloadVideos'], true),
-  //     downloadVideo,
-  //     skipDownload('Video downloads disabled', post)
-  //   )
-  // }
-
-  if (isArticleToSaveAsPdf(post)) {
-    return savePageAsPdf({ post, adminSettings, postMediaFolder })
+  if (isImagePost(post)) {
+    debugger
+    return Promise.resolve()
+    // return downloadImage({ post, adminSettings, postMediaFolder })
   }
 
+  debugger
+
+  if (isVideoPost(post)) {
+    debugger
+    return Promise.resolve()
+    // return adminSettings.downloadVideos
+    //   ? downloadVideo(post, adminSettings, postMediaFolder)
+    //   : skipDownload('Video downloads disabled', post)
+  }
+
+  debugger
+
+  if (isArticleToSaveAsPdf(post)) {
+    debugger
+    return savePageAsPdf(post, postMediaFolder)
+  }
+
+  debugger
+
   if (isTextPostWithNoUrlInPost(post)) {
+    debugger
     return skipDownload('Is a text-post with no url in post', post)
   }
 
+  debugger
   /*****
     Ignore crossposts for now where the url links to another post (eg https://www.reddit.com/r/...)
     Leave the crossposts check towards the end of the checks as it is sometimes possible to download
     the video or image of a crosspost if the url is not a https://www.reddit.com/r/ url.
   *****/
   if (isCrossPost(post)) {
+    debugger
     return skipDownload('Is a cross-post with no direct download url', post)
   }
 
+  debugger
   return skipDownload('No media match for download.', post)
 }
 
