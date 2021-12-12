807370c484768266b061ad45a4c09f56abbbe0fd
Coop <coop.coding@gmail.com>
2021-12-12 19:42:45 +1100

    Comments

--- downloads/media/log-download-errors.ts
+++ downloads/media/log-download-errors.ts
@@ -9,7 +9,7 @@ const genErrMessage = (permalink: string): string =>
   `Was unable to download media for post: https://www.reddit.com${permalink}`
 
 /*****
-  We want to log download errors, but only if they are errors unrelated to being offline.
+  logDownloadError is only called from download-posts-media when the error is unrelated to being offline.
   Also, we dont really need to log 404 not found errors as there will be lots of missing
    links (eg images that have been deleted).
 *****/
